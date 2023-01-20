4a7956d3ccf8862ea964eec5cc2bbf523df19c8c
cxr <claire_129as@yahoo.com.tw>
2023-01-21 06:21:19 +0800

    84d4b2a

---
+++ _nuxt/static/1674253251/movies/361743/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmovieappp\u002F_nuxt\u002Fstatic\u002F1674253251",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F361743",config:{_app:{basePath:"\u002Fmovieappp\u002F",assetsPath:"\u002Fmovieappp\u002F_nuxt\u002F",cdnURL:a}}}}(null));
